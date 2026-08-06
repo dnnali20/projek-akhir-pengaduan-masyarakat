122e516339cce1f95fa54ece1f29d71871f20c6f
Dennis Ali Fadillah <delif4.2010@gmail.com>
2026-08-06 23:18:45 +0700

    feat: update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,22 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-console.log(process.env.DB_HOST);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_NAME);
+console.log("HOST :", process.env.DB_HOST);
+console.log("USER :", process.env.DB_USER);
+console.log("PORT :", process.env.DB_PORT);
+console.log("DB   :", process.env.DB_NAME);
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT || 3306,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
   if (err) {
-    console.log(err);
+    console.log("Database gagal terkoneksi:", err);
   } else {
     console.log("Database berhasil terkoneksi");
   }
