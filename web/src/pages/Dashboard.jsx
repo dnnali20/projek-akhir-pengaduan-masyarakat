530f19b7066375a86ea20b2b19935a29e93d295d
Dennis Ali Fadillah <delif4.2010@gmail.com>
2026-08-07 23:47:50 +0700

    feat: perbaikan

--- web/src/pages/Dashboard.jsx
+++ web/src/pages/Dashboard.jsx
@@ -321,13 +321,10 @@ export default function Dashboard() {
                 {item.image ? (
                   
                   <img
-  src={`${API_URL}/uploads/${item.image}`}
-  alt={item.title}
-  className="h-52 w-full object-cover"
-  onError={(e) => {
-    console.log("Gagal load:", e.target.src);
-  }}
-/>
+                    src={`${API_URL}/uploads/${item.image}`}
+                    alt={item.title}
+                    className="h-52 w-full object-cover"
+                  />
                 ) : (
                   <div className="h-52 bg-gray-100 flex items-center justify-center">
                     <p className="text-gray-500">Tidak ada gambar</p>
